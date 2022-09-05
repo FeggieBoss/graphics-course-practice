b01ca32bf07bac4cfd407aa8cec63a3d6b1e78b3
FeggieBoss <tyuryaev2002@mail.ru>
2022-09-06 00:26:13 +0300

    Update main.cpp

--- 2022/practice1/main.cpp
+++ 2022/practice1/main.cpp
@@ -11,6 +11,45 @@
 #include <stdexcept>
 #include <iostream>
 
+
+const std::string fita = "fitusya";
+const std::string code1 = 
+R"(#version 330 core
+
+const vec2 VERTICES[3] = vec2[3](
+	vec2(0.0, 0.0),
+	vec2(1.0, 0.0),
+	vec2(0.0, 1.0)
+);
+
+out vec3 color;
+out float x;
+out float y;
+
+void main()
+{
+	gl_Position = vec4(VERTICES[gl_VertexID], 0.0, 1.0);
+	x = gl_Position.x;
+	y = gl_Position.y;
+	color = vec3(x,y,0.0);
+}
+)";
+const std::string code2 = 
+R"(#version 330 core
+layout (location = 0) out vec4 out_color;
+
+in vec3 color;
+in float x;
+in float y;
+
+void main()
+{
+	// out_color = vec4(color, 1.0); gradient task
+	float chess_color = (int(floor(x*16))&1) ^ ((int(floor(y*16))&1));
+	out_color = vec4(chess_color, chess_color, chess_color, 1.0);
+}
+)";
+
 std::string to_string(std::string_view str)
 {
 	return std::string(str.begin(), str.end());
@@ -24,6 +63,60 @@ void sdl2_fail(std::string_view message)
 void glew_fail(std::string_view message, GLenum error)
 {
 	throw std::runtime_error(to_string(message) + reinterpret_cast<const char *>(glewGetErrorString(error)));
+}
+
+
+GLuint create_shader(GLenum shader_type, const char* shader_source) {
+	GLuint shader = glCreateShader(shader_type);
+	
+	if(shader==0) {
+		std::cerr<<fita+(" says: create shader error")<<std::endl;
+		return EXIT_FAILURE;
+	}
+
+	glShaderSource(shader, 1, &shader_source, nullptr);
+	glCompileShader(shader);
+
+	GLint shader_compile_status;
+	glGetShaderiv(shader,GL_COMPILE_STATUS, &shader_compile_status);
+
+	if(shader_compile_status==GL_FALSE) {
+		GLsizei length_info_log;
+		GLsizei buffer_size;
+
+		std::string info_log_shader(buffer_size, '\0');
+		glGetShaderiv(shader,GL_INFO_LOG_LENGTH, &buffer_size);
+
+		glGetShaderInfoLog(shader,buffer_size,&length_info_log, const_cast<GLchar*>(info_log_shader.c_str()));
+
+		throw std::runtime_error(fita+" says: "+info_log_shader);
+	}
+
+	return shader;
+}
+
+GLuint create_program(GLuint vertex_shader, GLuint fragment_shader) {
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+	
+	GLint program_link_status;
+	glGetShaderiv(program,GL_LINK_STATUS, &program_link_status);
+
+	if(program_link_status==GL_FALSE) {
+		GLsizei length_info_log;
+		GLsizei buffer_size;
+
+		std::string info_log_program(buffer_size, '\0');
+		glGetShaderiv(program,GL_INFO_LOG_LENGTH, &buffer_size);
+
+		glGetShaderInfoLog(program,buffer_size,&length_info_log, const_cast<GLchar*>(info_log_program.c_str()));
+
+		throw std::runtime_error(fita+" says: "+info_log_program);
+	}
+
+	return program;
 }
 
 int main() try
@@ -57,6 +150,44 @@ int main() try
 
 	glClearColor(0.8f, 0.8f, 1.f, 0.f);
 
+
+
+
+
+
+
+
+
+
+
+	//
+	auto program = create_program(
+		create_shader(GL_VERTEX_SHADER, code1.c_str()),
+		create_shader(GL_FRAGMENT_SHADER, code2.c_str())
+	);
+
+	GLuint array;
+	glGenVertexArrays(1,&array);
+
+	glUseProgram(program);
+	glBindVertexArray(array);		
+
+	glProvokingVertex(GL_FIRST_VERTEX_CONVENTION);
+	//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	bool running = true;
 	while (running)
 	{
@@ -71,6 +202,21 @@ int main() try
 			break;
 
 		glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+
+
+
+		//
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//
+
+
+
+
+
+
 
 		SDL_GL_SwapWindow(window);
 	}
