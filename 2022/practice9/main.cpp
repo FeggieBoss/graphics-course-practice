858ce6d6badbc72f0ee2eafa6c4962bfad651364
Илья Тюряев <tyuryaev2002@mail.ru>
2022-11-01 19:35:27 +0300

    fix task4, publish tasks 5,6

--- 2022/practice9/main.cpp
+++ 2022/practice9/main.cpp
@@ -87,27 +87,59 @@ void main()
     shadow_pos /= shadow_pos.w;
     shadow_pos = shadow_pos * 0.5 + vec4(0.5);
 
-    float bias = 0.12;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    // task2 
+    float bias = 0.05;
 
     bool in_shadow_texture = (shadow_pos.x > 0.0) && (shadow_pos.x < 1.0) && (shadow_pos.y > 0.0) && (shadow_pos.y < 1.0) && (shadow_pos.z > 0.0) && (shadow_pos.z < 1.0);
     float shadow_factor = 1.0;
-    if (in_shadow_texture)
-        shadow_factor = (bias + texture(shadow_map, shadow_pos.xy).r < shadow_pos.z) ? 0.0 : 1.0;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    
 
+    float factor = 1.0;
     vec3 albedo = vec3(1.0, 1.0, 1.0);
+    if (in_shadow_texture) {
+        // task2 : shadow_factor = (bias + texture(shadow_map, shadow_pos.xy).r < shadow_pos.z) ? 0.0 : 1.0;
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////
-    // task4
-    vec2 data = texture(shadow_map, shadow_pos.xy).rg;
-    float mu = data.r;
-    float sigma = data.g - mu * mu;
-    float z = shadow_pos.z;
-    float factor = (z < mu) ? 1.0 : sigma / (sigma + (z - mu) * (z - mu));
-    //////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        // task4
+        float bias2 = -0.005;
+        
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        // task6
+        vec2 sum = vec2(0.0);
+        float sum_w = 0.0;
+        const int N = 5;
+        float radius = 3.0;
+        for (int x = -N; x <= N; ++x) {
+            for (int y = -N; y <= N; ++y) {
+                float c = exp(-float(x*x + y*y) / (radius*radius));
+                sum += c * texture2D(shadow_map, shadow_pos.xy + vec2(x,y) / vec2(textureSize(shadow_map, 0))).xy;
+                sum_w += c;
+            }
+        }
+        vec2 data = sum / sum_w;
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        float mu = data.r;
+        float sigma = data.g - mu * mu;
+        float zz = shadow_pos.z;
+        factor = (bias2 + zz < mu) ? 1.0 : sigma / (sigma + (zz - mu) * (zz - mu));
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        // task5
+        float delta = 0.125;
+        if(factor<delta) {
+            factor = 0;
+        }
+        else {
+            factor = (factor-delta) * 1.f/(1-delta);
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+    }
 
 
-    vec3 light = factor * ambient;
-    light += light_color * max(0.0, dot(normal, light_direction)) * shadow_factor;
+    vec3 light = ambient;
+    light += light_color * max(0.0, dot(normal, light_direction)) * factor;
     vec3 color = albedo * light;
 
     out_color = vec4(color, 1.0);
@@ -172,8 +204,9 @@ in vec4 gl_FragCoord;
 layout (location = 0) out vec4 z_zz;
 
 void main()
-{
-    z_zz = vec4(gl_FragCoord.z, gl_FragCoord.z * gl_FragCoord.z, 0.0, 0.0);
+{   
+    float z = gl_FragCoord.z;
+    z_zz = vec4(z, z * z + 0.25 * (dFdx(z)*dFdx(z) + dFdy(z)*dFdy(z)), 0.0, 0.0); // task5 
 }
 )";
 
